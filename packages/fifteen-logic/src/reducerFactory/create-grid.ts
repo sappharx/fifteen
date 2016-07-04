7145e4fa502b9b451665661dd182c53139033bae
sappharx <vinnysisk@gmail.com>
2016-07-04 11:24:48 -0600

    fix lint issues

--- packages/fifteen-logic/src/reducerFactory/create-grid.ts
+++ packages/fifteen-logic/src/reducerFactory/create-grid.ts
@@ -2,13 +2,13 @@ type Grid = Array<Array<number>>
 
 // QUESTION: do we want '0' to always be in the bottom right corner??
 export default function createGrid(gridSize: number): Grid {
-  let randomArray = createRandomizedArray(gridSize)
+  let randomArray: Array<number> = createRandomizedArray(gridSize)
   let gridArray: Grid = []
 
-  for (let i = 0; i < gridSize; ++i) {
-    const start = i * gridSize
-    const end = (i + 1) * gridSize
-    const array = randomArray.slice(start, end)
+  for (let i: number = 0; i < gridSize; ++i) {
+    const start: number = i * gridSize
+    const end: number = (i + 1) * gridSize
+    const array: Array<number> = randomArray.slice(start, end)
 
     gridArray = gridArray.concat([array])
   }
@@ -17,12 +17,12 @@ export default function createGrid(gridSize: number): Grid {
 }
 
 function createRandomizedArray(gridSize: number): Array<number> {
-  let availableNumbers = createBasicArray(gridSize)
+  let availableNumbers: Array<number> = createBasicArray(gridSize)
   let randomArray: Array<number> = []
 
   while (availableNumbers.length > 0) {
-    const randomIndex = getRandomIntInclusive(0, availableNumbers.length - 1)
-    const value = availableNumbers.splice(randomIndex, 1)
+    const index: number = getRandomIntInclusive(0, availableNumbers.length - 1)
+    const value: Array<number> = availableNumbers.splice(index, 1)
 
     randomArray = randomArray.concat(value)
   }
@@ -48,7 +48,7 @@ function getRandomIntInclusive(min: number, max: number): number {
 }
 
 function isPathologicalArray(array: Array<number>): boolean {
-  for (let i = 0, j = array.length - 1; j > 0; ++i, --j) {
+  for (let i: number = 0, j: number = array.length - 1; j > 0; ++i, --j) {
     if (array[i] !== j) {
       return false
     }
