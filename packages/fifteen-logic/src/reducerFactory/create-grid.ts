b9e382c864d80b4688e7ccace397df4b7ac4fa3b
Vincent Sisk <vincent.sisk@workday.com>
2016-07-03 18:12:52 -0600

    refactor createGrid

--- packages/fifteen-logic/src/reducerFactory/create-grid.ts
+++ packages/fifteen-logic/src/reducerFactory/create-grid.ts
@@ -1,39 +1,45 @@
-export default function createGrid(size:number): Array<Array<number>> {
-  let availableNumbers = createBasicArray(size)
+type Grid = Array<Array<number>>
+
+// QUESTION: do we want '0' to always be in the bottom right corner??
+export default function createGrid(gridSize: number): Grid {
+  let randomArray = createRandomizedArray(gridSize)
+  let gridArray: Grid = []
+
+  for (let i = 0; i < gridSize; ++i) {
+    const start = i * gridSize
+    const end = (i + 1) * gridSize
+    const array = randomArray.slice(start, end)
+
+    gridArray = gridArray.concat([array])
+  }
+
+  return gridArray
+}
+
+function createRandomizedArray(gridSize: number): Array<number> {
+  let availableNumbers = createBasicArray(gridSize)
   let randomArray: Array<number> = []
-  
 
   while (availableNumbers.length > 0) {
     const randomIndex = getRandomIntInclusive(0, availableNumbers.length - 1)
     const value = availableNumbers.splice(randomIndex, 1)
-    randomArray.push(value[0])
+
+    randomArray = randomArray.concat(value)
   }
 
-  function createGridArray(size:number) {
-    let gridArray: Array<Array<number>> = []
-    let count: number = size
-
-    while (count > 0) {
-      const array = randomArray.splice(0, size)
-      gridArray.push(array)
-      count--
-    }
-
-    return gridArray
-  }
-  
-  return createGridArray(size)
+  return randomArray
 }
 
-function getRandomIntInclusive(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomIntInclusive(min: number, max: number): number {
+  return min + Math.floor(Math.random() * (max - min + 1));
 }
 
-function createBasicArray(size:number): Array<number> {
-  let length:number = size * size
+function createBasicArray(size: number): Array<number> {
+  let numbersRemaining: number = size * size
   let array: Array<number> = []
-  while (length > 0) {
-    array.push(--length)
+
+  while (numbersRemaining > 0) {
+    array = array.concat(--numbersRemaining)
   }
 
   return array
